a34e42b1dbc1dccded1629c77c5622fdc85ce8a8
Kappro <limtkglenn@gmail.com>
2026-02-26 15:28:17 +0800

    Manually updated checksums for v0.3.1.

--- Formula/geoengine.rb
+++ Formula/geoengine.rb
@@ -4,30 +4,30 @@
 class Geoengine < Formula
   desc "Docker-based isolated runtime manager for geospatial workloads"
   homepage "https://github.com/NikaGeospatial/geoengine"
-  version "0.3.0"
+  version "0.3.1"
   license "MIT"
 
   on_macos do
     on_intel do
       url "https://github.com/NikaGeospatial/geoengine/releases/download/v#{version}/geoengine-darwin-x86_64.tar.gz"
-      sha256 "0ab8ce9afb49df334d2b92387a446cb3a0f497db95aa827de5e2adedb3156882"
+      sha256 "50eb2b557de9fc46c186b5dcacf6b93c0788a27b1e1fb890c35ac61abf311dfb"
     end
 
     on_arm do
       url "https://github.com/NikaGeospatial/geoengine/releases/download/v#{version}/geoengine-darwin-aarch64.tar.gz"
-      sha256 "0351bc114de3e8a584557d474091263704b16360612946d061c5260518472d5c"
+      sha256 "3b4b9201e575cf99aa3622a5bb3ee190bac82d959b4482d042104750a3e929fb"
     end
   end
 
   on_linux do
     on_intel do
       url "https://github.com/NikaGeospatial/geoengine/releases/download/v#{version}/geoengine-linux-x86_64.tar.gz"
-      sha256 "f601fff2f57f3a24ca3b85fb5e97eab6af009428333f75434687c741d94d98d8"
+      sha256 "51b56e042bf2b82fee7ca9abf7ec8e42a9794d74437884c97c4d57ce50347357"
     end
 
     on_arm do
       url "https://github.com/NikaGeospatial/geoengine/releases/download/v#{version}/geoengine-linux-aarch64.tar.gz"
-      sha256 "6fd5692f774800d78b0d0626cd1fe0703dd27c5d66f50abd26f7c5838c39dcd7"
+      sha256 "aa3c284351fe6be166f55c38239182b4fea85c08ebf26406bc3cf8579de6e0f7"
     end
   end
 
